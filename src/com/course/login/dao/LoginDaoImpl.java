59862e9954658749e13fc54fd5cf7077c03b14df
tonghaiping <2362264483@qq.com>
2016-11-26 20:32:28 +0800

    feature:用户注册、登录、编辑个人信息功能

--- src/com/course/login/dao/LoginDaoImpl.java
+++ src/com/course/login/dao/LoginDaoImpl.java
@@ -6,13 +6,73 @@ import com.framework.BaseDao;
 
 @Repository
 public class LoginDaoImpl extends BaseDao<StudentInfo, Integer>{
-	
+	/**
+	 * 
+	 * @Description 		根据姓名密码查询学生
+	 * @author 				童海苹
+	 * @createDate  		2016/11/22
+	 * @version 			V1.0
+	 * 
+	 */
 	public StudentInfo findByNameAndPwd(String name, String pwd){
 		try {
 			return super.findOne("from StudentInfo lu where lu.loginName=? and lu.password=?", new Object[]{name,pwd});
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
+		}
+	}
+	
+	/**
+	 * 
+	 * @Description 		根据姓名查询学生
+	 * @author 				童海苹
+	 * @createDate  		2016/11/22
+	 * @version 			V1.0
+	 * 
+	 */
+	public StudentInfo findByName(String name){
+		try {
+			return super.findOne("from StudentInfo s where s.loginName=?", new Object[]{name});
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * 
+	 * @Description 		根据学生ID查询学生
+	 * @author 				童海苹
+	 * @createDate  		2016/11/22
+	 * @version 			V1.0
+	 * 
+	 */
+	public StudentInfo getStudentInfo(int studentId){
+		StudentInfo s = null;
+		try {
+			s = this.get(studentId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return s;
+	}
+	
+	/**
+	 * 
+	 * @Description 		更新学生信息
+	 * @author 				童海苹
+	 * @createDate  		2016/11/22
+	 * @version 			V1.0
+	 * 
+	 */
+	public void updateStudentInfo(StudentInfo s){
+		try {
+			System.out.println("updateStudentInfoId:"+s.getStudentId()+"updateStudentInfo:"+s.getHobby());
+			this.update(s);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
--- src/com/course/login/service/LoginServiceImpl.java
+++ src/com/course/login/service/LoginServiceImpl.java
@@ -3,30 +3,90 @@ package com.course.login.service;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.course.entity.StudentInfo;
 import com.course.login.dao.LoginDaoImpl;
 
 @Service
-@Transactional(readOnly = true)
 public class LoginServiceImpl {
 	
 	@Resource
 	private LoginDaoImpl loginDaoImpl;
 	
-	@Transactional(readOnly = true)
+	/**
+	 * 
+	 * @Description 		注册
+	 * @author 				童海苹
+	 * @createDate  		2016/11/22
+	 * @version 			V1.0
+	 * 
+	 */
 	public void regist(String name,String pwd){
 		StudentInfo student = new StudentInfo();
 		student.setLoginName(name);
 		student.setPassword(pwd);
+		student.setRoleName("student");
 		try {
 			this.loginDaoImpl.save(student);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * 
+	 * @Description 		根据姓名查询账户已存在
+	 * @author 				童海苹
+	 * @createDate  		2016/11/22
+	 * @version 			V1.0
+	 * 
+	 */
+	public StudentInfo checkStudent(String name){
+		return this.loginDaoImpl.findByName(name);
+	}
+	
+	/**
+	 * 
+	 * @Description 		登录
+	 * @author 				童海苹
+	 * @createDate  		2016/11/22
+	 * @version 			V1.0
+	 * 
+	 */
 	public StudentInfo login(String name, String pwd){
 		return this.loginDaoImpl.findByNameAndPwd(name, pwd);
+	}
+	
+	/**
+	 * 
+	 * @Description 		根据学生ID查找学生
+	 * @author 				童海苹
+	 * @createDate  		2016/11/22
+	 * @version 			V1.0
+	 * 
+	 */
+	public StudentInfo getStudentInfo(int studentId){
+		return this.loginDaoImpl.getStudentInfo(studentId);
+	}
+	
+	/**
+	 * 
+	 * @Description 		添加学生的属性
+	 * @author 				童海苹
+	 * @createDate  		2016/11/22
+	 * @version 			V1.0
+	 * 
+	 */
+	public void editStudentInfo(StudentInfo s){
+		StudentInfo stu = this.loginDaoImpl.getStudentInfo(s.getStudentId());
+		System.out.println("editStudentInfo:password"+stu.getPassword());
+		stu.setHobby(s.getHobby());
+		stu.setLocation(s.getLocation());
+		stu.setIntroduce(s.getIntroduce());
+		try {
+			this.loginDaoImpl.updateStudentInfo(stu);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
