a44c665227cc84c3702140fee57a403cf38a0477
dell <dell@10.7.89.77>
2016-12-01 20:50:55 +0800

    实现专项列表页的查询,controller,service,dao层

--- src/com/course/zx/controller/ExamListController.java
+++ src/com/course/zx/controller/ExamListController.java
@@ -1,10 +1,15 @@
 package com.course.zx.controller;
 
+import java.io.UnsupportedEncodingException;
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.course.entity.ParentQuestion;
+import com.course.parentquestion.service.ParentQuestionServiceImpl;
+import com.framework.Page;
 
 @Controller
 @RequestMapping("examzx")
@@ -12,19 +17,33 @@ public class ExamListController {
 	
 	/**
 	 * 
-	 * @desc				查看试卷，封装到page对象里  返回jsp页面
+	 * @desc				实现依照大题名称获取parentQuestion  返回内容方便specialprojectlist.jsp的内容动态获取
 	 * @author				翟佳帆
-	 * @createDate 			2016/11/22
-	 * @param 				pageNum页码，searchParam搜索参数
+	 * @param 				parentQuestionName 
+	 * @createDate 			2016/12/1
+	 * @param 				pageNum页码，parentQuestionName大题名称
 	 * @return				String
 	 * 
 	 */
+	@Resource
+	private ParentQuestionServiceImpl parentQuestionSeriviceImpl;
+	
 	@RequestMapping("list")
-	public String list(@RequestParam(name="parentQuestionName", defaultValue="") String parentQuestionName,
+	public String list(@RequestParam(name="pageNum", defaultValue="1") int pageNum,
+			@RequestParam(name="parentQuestionName",defaultValue="") String parentQuestionName,
 			HttpServletRequest request,
 			Model model){
 		
-//		实现这个函数  以及specialprojectlist.jsp的内容动态获取
+		Page<ParentQuestion> page;
+		
+		if( parentQuestionName== null || "".equals(parentQuestionName)){
+			page=this.parentQuestionSeriviceImpl.listParentQuestionExamByParentQuestionName(pageNum, 5, null);	
+		}else{
+			page=this.parentQuestionSeriviceImpl.listParentQuestionExamByParentQuestionName(pageNum, 5, new Object[]{parentQuestionName});
+		}
+		
+		request.setAttribute("page", page);
+		request.setAttribute("pqType",parentQuestionName);
 		return "examzx/specialprojectlist";
 	}
 	
