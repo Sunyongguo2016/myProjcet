ed4568dd94df6c36d0e1b4afae780eb56bd2b900
liqiaochuv <li970127@126.com>
2016-12-01 11:13:02 +0800

    实现专项题目预览的控制器

--- src/com/course/zx/controller/ExamListController.java
+++ src/com/course/zx/controller/ExamListController.java
@@ -1,14 +1,22 @@
 package com.course.zx.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.course.entity.ParentQuestion;
+import com.course.parentquestion.service.ParentQuestionServiceImpl;
+import com.framework.Page;
+
 @Controller
 @RequestMapping("examzx")
 public class ExamListController {
+	
+	@Resource
+	private ParentQuestionServiceImpl parentQuestionServiceImpl;
 	
 	/**
 	 * 
@@ -32,19 +40,21 @@ public class ExamListController {
 	
 	/**
 	 * 
-	 * @desc				查看试卷，封装到page对象里  返回jsp页面
+	 * @desc				实现parentquestion的获取,返回zxpreview.jsp页面
 	 * @author				李翘楚
-	 * @createDate 			2016/11/22
-	 * @param 				pageNum页码，searchParam搜索参数
+	 * @createDate 			2016/12/1
+	 * @param 				parentQuestionId  大题id  
 	 * @return				String
 	 * 
 	 */
 	@RequestMapping("preview")
-	public String preview(@RequestParam(name="parentQuestionId", defaultValue="1") int parentQuestionId ,
-			HttpServletRequest request,
-			Model model){
+	public String preview(@RequestParam(name="parentQuestionId", defaultValue="2") int parentQuestionId ,
+				HttpServletRequest request,
+				Model model){
 		
-//		实现这个函数  以及zxpreview的动态获取内容
+		ParentQuestion parentQuestion = new ParentQuestion();
+		parentQuestion = this.parentQuestionServiceImpl.getParentQuestion(parentQuestionId);
+		request.setAttribute("parentQuestion", parentQuestion);
 		return "examzx/zxpreview";
 	}
 	
