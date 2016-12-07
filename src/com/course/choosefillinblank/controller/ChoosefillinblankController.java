ab4118d6b7cfc53553baf2c68f5a69722af59648
Tianruihang <dell@dell-PC>
2016-12-07 17:13:01 +0800

    快速阅读、阅读、十五选十代码优化

--- src/com/course/choosefillinblank/controller/ChoosefillinblankController.java
+++ src/com/course/choosefillinblank/controller/ChoosefillinblankController.java
@@ -54,7 +54,7 @@ public class ChoosefillinblankController {
 	 * 
 	 * @desc				添加十五选十大题，获取addcontent.jsp的参数，实现添加parentQuestion对象；
 	 * @author				孙永国
-	 * @createDate 			2016/11/21
+	 * @createDate 			2016/11/21 修改日期 2016/12/7 修改者：田瑞航
 	 * @param 				获得添加十五选十大题相关参数
 	 * @return				String
 	 * 
@@ -67,96 +67,14 @@ public class ChoosefillinblankController {
 			@RequestParam(name = "parentquestionarticle") String parentQuestionArticle,
 			@RequestParam(name = "parentquestiontitle") String parentQuestionTitle,
 			@RequestParam(name = "description") String description,
-			@RequestParam(name = "aselecttcontent") String aSelecttContent,
-			@RequestParam(name = "bselecttcontent") String bSelecttContent,
-			@RequestParam(name = "cselecttcontent") String cSelecttContent,
-			@RequestParam(name = "dselecttcontent") String dSelecttContent,
-			@RequestParam(name = "eselecttcontent") String eSelecttContent,
-			@RequestParam(name = "fselecttcontent") String fSelecttContent,
-			@RequestParam(name = "gselecttcontent") String gSelecttContent,
-			@RequestParam(name = "hselecttcontent") String hSelecttContent,
-			@RequestParam(name = "iselecttcontent") String iSelecttContent,
-			@RequestParam(name = "jselecttcontent") String jSelecttContent,
-			@RequestParam(name = "kselecttcontent") String kSelecttContent,
-			@RequestParam(name = "lselecttcontent") String lSelecttContent,
-			@RequestParam(name = "mselecttcontent") String mSelecttContent,
-			@RequestParam(name = "nselecttcontent") String nSelecttContent,
-			@RequestParam(name = "oselecttcontent") String oSelecttContent,
 			@RequestParam(name = "questionscore") String questionScore,
-			@RequestParam(name = "questionanswer36") String questionAnswer36,
-			@RequestParam(name = "questionanswer37") String questionAnswer37,
-			@RequestParam(name = "questionanswer38") String questionAnswer38,
-			@RequestParam(name = "questionanswer39") String questionAnswer39,
-			@RequestParam(name = "questionanswer40") String questionAnswer40,
-			@RequestParam(name = "questionanswer41") String questionAnswer41,
-			@RequestParam(name = "questionanswer42") String questionAnswer42,
-			@RequestParam(name = "questionanswer43") String questionAnswer43,
-			@RequestParam(name = "questionanswer44") String questionAnswer44,
-			@RequestParam(name = "questionanswer45") String questionAnswer45,
-			@RequestParam(name = "questionexplain36") String questionExplain36,
-			@RequestParam(name = "questionexplain37") String questionExplain37,
-			@RequestParam(name = "questionexplain38") String questionExplain38,
-			@RequestParam(name = "questionexplain39") String questionExplain39,
-			@RequestParam(name = "questionexplain40") String questionExplain40,
-			@RequestParam(name = "questionexplain41") String questionExplain41,
-			@RequestParam(name = "questionexplain42") String questionExplain42,
-			@RequestParam(name = "questionexplain43") String questionExplain43,
-			@RequestParam(name = "questionexplain44") String questionExplain44,
-			@RequestParam(name = "questionexplain45") String questionExplain45,
 			HttpServletRequest request) {
-		
-		
-		//用集合！！很好用的方法！
-		//用集合获取参数  再在for循环中使用 方便利用for循环添加question
-		
-		//list集合存储所有的question 答案和解析 信息   方便在for循环中存储
-		List<String> questionLists = new ArrayList<String>();
-		questionLists.add(questionAnswer36);
-		questionLists.add(questionAnswer37);
-		questionLists.add(questionAnswer38);
-		questionLists.add(questionAnswer39);
-		questionLists.add(questionAnswer40);
-		questionLists.add(questionAnswer41);
-		questionLists.add(questionAnswer42);
-		questionLists.add(questionAnswer43);
-		questionLists.add(questionAnswer44);
-		questionLists.add(questionAnswer45);
-		questionLists.add(questionExplain36);
-		questionLists.add(questionExplain37);
-		questionLists.add(questionExplain38);
-		questionLists.add(questionExplain39);
-		questionLists.add(questionExplain40);
-		questionLists.add(questionExplain41);
-		questionLists.add(questionExplain42);
-		questionLists.add(questionExplain43);
-		questionLists.add(questionExplain44);
-		questionLists.add(questionExplain45);
-	
-		
-		//用list集合保存单选内容   方便用循环存储数据
-		List<String> selecttLists = new ArrayList<String>();
-		selecttLists.add(aSelecttContent);
-		selecttLists.add(bSelecttContent);
-		selecttLists.add(cSelecttContent);
-		selecttLists.add(dSelecttContent);
-		selecttLists.add(eSelecttContent);
-		selecttLists.add(fSelecttContent);
-		selecttLists.add(gSelecttContent);
-		selecttLists.add(hSelecttContent);
-		selecttLists.add(iSelecttContent);
-		selecttLists.add(jSelecttContent);
-		selecttLists.add(kSelecttContent);
-		selecttLists.add(lSelecttContent);
-		selecttLists.add(mSelecttContent);
-		selecttLists.add(nSelecttContent);
-		selecttLists.add(oSelecttContent);
 		
 		// 根据examName找到exam对象
 		Exam exam = this.examServiceImpl.findByName(examName);
 		if (exam == null) {
 			return "404";
 		}
-		
 		
 		//将获取到的参数赋值给parentquestion,保存parentquestion
 		ParentQuestion parentQuestion = new ParentQuestion();
@@ -174,27 +92,26 @@ public class ChoosefillinblankController {
 		
 		
 		//存储question表     根据传参 questionfrom questionto 决定循环次数 决定存储多少question
-		for (Integer i = 0; i < questionTo-questionFrom+1; i++) {
+		for (Integer i = 1; i <= questionTo-questionFrom+1; i++) {
 			Question question = new Question();
+			//获取并设置参数
 			question.setParentQuestion(parentQuestion);
-			question.setQuestionContent(((Integer)(i+questionFrom)).toString());
-			question.setQuestionAnswer(questionLists.get(i));
-			
-			//集合questionLists中  前十个是answer  后十个是explain
-			question.setQuestionExplain(questionLists.get(i+10));
+			question.setQuestionContent(((Integer)(questionFrom+i-1)).toString());
+			question.setQuestionAnswer(request.getParameter("questionanswer"+i));
+			question.setQuestionExplain(request.getParameter("questionexplain"+i));
 			question.setQuestionScore(Float.parseFloat(questionScore));
 			
 			List<Selectt> selectts = new ArrayList<Selectt>(0);
 			
 			//存储selectt表  将selectts赋值给question 与question联系起来
-			for(Integer j = 0; j<15; j++){
+			for(Integer j = 1; j<16; j++){
 				Selectt selectt = new Selectt();
 				selectt.setQuestion(question);
-				char c=(char) (j+65);
+				char c=(char) (j+64);
 				
 				//根据ASCII码转换成字符'A' 'B'..
 				selectt.setSelecttName(c+"");
-				selectt.setSelecttContent(selecttLists.get(j));
+				selectt.setSelecttContent(request.getParameter("selecttcontent"+j));
 				
 				this.selecttServiceImpl.addSelectt(selectt);
 				selectts.add(selectt);
---
+++ src/com/course/util/AuthFilter.java
@@ -0,0 +1,64 @@
+package com.course.util;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+	/**
+	 * 
+	 * @desc            过滤器（拦截器）
+	 * @param           查看用户是否登陆过，未登录禁止访问页面
+	 * @author          田瑞航
+	 * @createDate      2016/12/5
+	 * @return          void
+	 * 
+	 */
+
+	public class AuthFilter implements Filter {
+		
+		//销毁
+	    @Override
+	    public void destroy() {
+	    }
+	    @Override
+	    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+	            throws IOException, ServletException {
+	       
+	    	HttpServletRequest request = (HttpServletRequest) servletRequest;
+	        HttpServletResponse response = (HttpServletResponse) servletResponse;
+	        //获取根目录所对应的绝对路径
+	        String currentURL = request.getRequestURI();
+	        //截取到当前文件名用于比较
+	        String targetURL = currentURL.substring(currentURL.indexOf("/",1),currentURL.length());
+	        //System.out.println(targetURL);
+	        //如果session不为空就返回该session，如果为空就返回null
+	        HttpSession session = request.getSession(false);
+	        if(!"/login_use.jsp".equals(targetURL)){
+	            //判断当前页面是否是重定参后的登陆页面页面，如果是就不做session的判断，防止死循环
+	            if(session==null||session.getAttribute("user")==null){
+	                //如果session为空表示用户没有登陆就重定向到404.jsp页面
+	                //System.out.println("request.getContextPath()=" + request.getContextPath());  
+	                response.sendRedirect(request.getContextPath()+"/login_use.jsp");
+	                return;
+	            }
+	        }
+	
+	        //继续向下执行
+	        chain.doFilter(request, response);
+	    }
+	
+	    /**
+	     * 初始化
+	     */
+	    @Override
+	    public void init(FilterConfig arg0) throws ServletException {
+	    }
+	
+	}
