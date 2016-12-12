73c2117956d940fefb06d13b3518db0ba64131c5
tonghaiping <2362264483@qq.com>
2016-12-12 19:03:30 +0800

    fix:修改了设置页面的js和验证代码

--- WebContent/js/reset.js
+++ WebContent/js/reset.js
@@ -1,32 +1,23 @@
-	function check(form){
-			if (form.password.value=="") {
-				alert("请输入新密码！");
-				form.password.focus();
-				return false;
-			}
-			if(form.oldPwd.value=="") {
-				alert("请输入旧密码！");
-				form.oldPwd.focus();
-				return false;
-			}
-			if(form.surepassword.value==""){
-				alert("请输入确认密码！");
-				form.surepassword.focus();
-				return false;
-			}
-			
-			if (form.password.value.length<6||form.password.value.length>100) {
-				alert("输入密码长度不对！");
-				form.password.focus();
-				return false;
-			};
-			if(form.password.value == form.surepassword.value){
-				return true;
-			}else{
-				alert("密码和确认密码不一致！");
-				form.surepassword.focus();
-				return false;
-			}
-			return true;
-		}
-	
+function checkPwd(){
+	var p = document.getElementById("password");
+	var pwd = document.getElementById("password").value;
+
+	if(pwd == ""){
+		alert("新密码不可为空！");
+		p.focus();
+	} else if(pwd.length<6 || pwd.lengtth>100){
+		alert("新密码长度不对！")
+		p.focus();
+	}
+}
+function checkONPwd(){
+	var rp = document.getElementById("password");
+	var pwd = document.getElementById("password").value;
+	var repwd = document.getElementById("repassword").value;
+	if(pwd != repwd){
+		document.getElementById("password").focus();
+		alert("密码和确认密码不一致！")
+	}
+}
+
+
