667f830ffb0e63edb430d87b7bf6c146e207af4e
Sunyongguo2016 <1241069301@qq.com>
2016-12-20 16:17:45 +0800

    fix: 限制用户名和密码长度

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -9,12 +9,12 @@
 				form.pwd.focus();
 				return false;
 			}
-			if (form.uer.value.length<5||form.uer.value.length>100) {
+			if (form.uer.value.length<5||form.uer.value.length>10) {
 				alert("输入用户名长度不对！");
 				form.uer.focus();
 				return false;
 			}
-			if (form.pwd.value.length<6||form.pwd.value.length>100) {
+			if (form.pwd.value.length<6||form.pwd.value.length>10) {
 				alert("输入密码长度不对！");
 				form.pwd.focus();
 				return false;
